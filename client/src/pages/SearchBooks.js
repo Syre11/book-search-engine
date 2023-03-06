42a992e19739853934058b70cdac20f3526b0d61
“Spencer <srye1140@gmail.com>
2023-03-05 18:47:54 -0800

    Adds front and back end work, with bugs

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -12,6 +12,9 @@ import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
+import { SAVE_BOOK } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
+
 const SearchBooks = () => {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
@@ -20,6 +23,7 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+  const [saveBook] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -72,11 +76,10 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const { data } = await saveBook({
+        variables: { newBook: { ...bookToSave } },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
---
+++ server/schemas/resolvers.js
@@ -0,0 +1,84 @@
+const { GraphQLError } = require("graphql");
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).select('-__v -password').populate('books');
+      }
+      throw new GraphQLError("You need to be logged in!", {
+        extensions: {
+          code: "UNAUTHENTICATED",
+        },
+      });
+    }
+  },
+
+  Mutation: {
+    loginUser: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new GraphQLError("No profile with this email found!", {
+          extensions: {
+            code: "UNAUTHENTICATED",
+          },
+        });
+      }
+      
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new GraphQLError("Incorrect password!", {
+          extensions: {
+            code: "UNAUTHENTICATED",
+          },
+        });
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          { _id: context.user._id },
+          { $addToSet: { savedBooks: args.input } },
+          { new: true, runValidators: true }
+        );
+        return updatedUser;
+      }
+      throw new GraphQLError("You need to be logged in!", {
+        extensions: {
+          code: "UNAUTHENTICATED",
+        },
+      });
+    },
+    removeBook: async (parent, args, context) => {
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId: args.bookId } } },
+          { new: true }
+        );
+
+        return updatedUser
+      }
+      throw new GraphQLError("You need to be logged in!", {
+        extensions: {
+          code: "UNAUTHENTICATED",
+        },
+      });
+    },
+  },
+};
+
+module.exports = resolvers;
